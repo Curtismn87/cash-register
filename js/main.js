707fa6128764f2fdce7c597939b6ecece0e345aa
Matt Curtis <Curtismn87@yahoo.com>
2015-09-02 15:38:29 -0400

    added JS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// var userinput
+var userInput = document.getElementById("newEntry")
+//var form
+var form = document.getElementById("entry")
+var total = document.getElementById("total")
+// when the user submits a form
+form.addEventListener("submit", function(event){
+  event.preventDefault()
+// console log the userinput
+console.log(userInput.value)
+// get the total value
+var currentTotal = total.innerHTML.
+console.log(total.innerHTML.substr(1))
+// add the user input to the total value
+var totalValue = total.innerHTML + userInput.value
+console.log(totalValue)
+// update the total value in the html
+})
